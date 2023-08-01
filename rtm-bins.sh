67794de7200f9f555a9ecc070076891cf75b8620
kebabas1600 <85343176+kebabas1600@users.noreply.github.com>
2023-08-01 14:52:07 +0100

    Update rtm-bins.sh

--- rtm-bins.sh
+++ rtm-bins.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-COIN_NAME='raptoreum'
-COIN_DAEMON='raptoreumd'
-COIN_CLI='raptoreum-cli'
-COIN_TX='raptoreum-tx'
+COIN_NAME='neoxa'
+COIN_DAEMON='neoxad'
+COIN_CLI='neoxa-cli'
+#COIN_TX='raptoreum-tx'
 COIN_PATH='/usr/local/bin'
-WALLET_TAR=$(curl -s https://api.github.com/repos/Raptor3um/raptoreum/releases/latest | jq -r '.assets[] | select(.name|test("ubuntu20.")) | .browser_download_url')
+WALLET_TAR=$(curl -s https://api.github.com/repos/NeoxaChain/Neoxa/releases/latest | jq -r '.assets[] | select(.name|test("linux64.")) | .browser_download_url')
 
 # fetch latest release using github api
 if pgrep $COIN_DAEMON; then
   $COIN_CLI stop
   mkdir temp
-  curl -L $WALLET_TAR | tar xz -C ./temp; mv ./temp/$COIN_DAEMON ./temp/$COIN_CLI ./temp/$COIN_TX $COIN_PATH
+  curl -L $WALLET_TAR | tar xz -C ./temp; mv ./temp/$COIN_DAEMON ./temp/$COIN_CLI $COIN_PATH
   $COIN_DAEMON
 else
   mkdir temp
-  curl -L $WALLET_TAR | tar xz -C ./temp; mv ./temp/$COIN_DAEMON ./temp/$COIN_CLI ./temp/$COIN_TX $COIN_PATH
+  curl -L $WALLET_TAR | tar xz -C ./temp; mv ./temp/$COIN_DAEMON ./temp/$COIN_CLI $COIN_PATH
   rm -rf temp
 fi
  
